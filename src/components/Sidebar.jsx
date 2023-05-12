771263e55a0c81473d400cf78d848f306726f859
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-05-12 18:48:40 -0300

    feed api and rendering the videos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,14 +3,17 @@ import { Stack } from '@mui/material';
 
 import { categories } from '../utils/constants';
 
-const selectedCategory = 'New';
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+    const selectedCategory = props.selectedCategory;
 
+    const handleChangeSelectedCategory = (cat) => {
+        props.onChangeSelectedCategory(cat);
+    }
     return (
         <Stack direction="row" sx={{overflowY: 'auto', height: {sx: 'auto', md: '95%'}, flexDirection: {md: 'column'}}}>
             {categories.map((cat) => (
-                <button key={cat.name} className="category-btn" style={{backgroundColor: cat.name === selectedCategory && '#fc1503', color: '#fff' }}>
+                <button onClick={() => handleChangeSelectedCategory(cat.name)} key={cat.name} className="category-btn" style={{backgroundColor: cat.name === selectedCategory && '#fc1503', color: '#fff' }}>
                     <span style={{color: cat.name === selectedCategory ? 'white' : 'red', marginRight: '10px'}}>{cat.icon}</span>
                     <span style={{opacity: cat.name === selectedCategory ? '1' : '0.8'}}>{cat.name}</span>
                 </button>
