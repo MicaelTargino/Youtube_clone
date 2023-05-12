e319cc37289a0177ef9668aa9d355277294b78c7
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-05-12 15:52:50 -0300

    starts the implementation of feed section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import {Box, Stack, Typography} from '@mui/material';
-import { Sidebar } from './index.js'
+import { Sidebar, Videos } from './index.js'
 const Feed = () => {
     const date = new Date();
     const atual_year = date.getFullYear();
@@ -13,6 +13,12 @@ const Feed = () => {
                 <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
                     Copyright {atual_year} JSM Media
                 </Typography>
+            </Box>
+            <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+                <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white'}}>
+                    New <span style={{color: '#f31503'}}>videos</span>
+                </Typography>
+                <Videos videos={[]} />
             </Box>
         </Stack>
     )}
