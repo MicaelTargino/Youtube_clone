d57695f6f7dd07d4f6158de5c6e8072faed081df
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-05-12 23:40:03 -0300

    search functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState();
+    const nav = useNavigate();
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        searchTerm && nav(`search/${searchTerm}`)
+    }
     return (
         <Paper 
             component='form' 
-            onSubmit={() => {}} 
+            onSubmit={handleSubmit} 
             sx={{borderRadius: 20, border: '1px solid #e3e3e3', pl:2, boxshadow: 'none', mr: {sm:5} }} 
             >
             <input 
                 type="text"
                 className='search-bar'
                 placeholder='Search...'
-                value=""
-                onChange={() => {}}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
             />
             <IconButton type="submit" sx={{ p:'10px', color: 'red' }}>
                 <Search />
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
-const SearchFeed = () => (
-    <div>SearchFeed</div>
-)
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import {Box, Typography} from '@mui/material';
+import { Videos } from './index.js'
+const SearchFeed = () => {
+    const [videos, setVideos] = useState([]);
+    const { searchTerm } = useParams();
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+        .then(data => setVideos(data.items))
+    }, [searchTerm]);
+
+    return (
+        <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2}}>
+            <Typography variant="h4" fontWeight="bold" mb={2} sx={{color: 'white'}}>
+    Search Results for: <span style={{color: '#f31503'}}>{searchTerm}</span> videos
+            </Typography>
+            <Videos videos={videos} />
+        </Box>
+    )}
 
 export default SearchFeed;
